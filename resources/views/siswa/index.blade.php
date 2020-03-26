8a0b149944cc20af45a86031a5e2173b7bd6ee15
Rivera Light <riveralight@aol.com>
2020-03-27 00:35:38 +0700

    Edit judul tambah data siswa baru

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -65,7 +65,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="staticBackdropLabel">Tambah Data Siswa</h5>
+                <h5 class="modal-title" id="staticBackdropLabel">Tambah Data Siswa Baru</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
